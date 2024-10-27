b48ec65218eb79046248b8025cee1ef80bdcb0b4
Bice Marzagora <59375165+BiceMar@users.noreply.github.com>
2024-10-27 12:13:51 +0100

    Fixed BCD counter for hours and minutes

--- ClockVHDL.srcs/sources_1/new/Design.vhd
+++ ClockVHDL.srcs/sources_1/new/Design.vhd
@@ -1,207 +1,189 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.std_logic_unsigned.all;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.std_logic_unsigned.ALL;
 
-entity clock is
-    port (
-        clk, rst : in std_logic;
-        b1, b2, b3, b4 : in std_logic;
-        d1, d2, d3, d4 : out std_logic_vector(6 downto 0)
-    );
-end clock;
-
-architecture hardware of clock is
-    signal count_sec : integer range 0 to 59; -- seconds counter (0 to 59)
-    signal count_clock : integer range 0 to 9; -- clock cycles counter (0 to 9)
-    signal m_u : integer range 0 to 9; -- minutes units (0 to 9)
-    signal m_d : integer range 0 to 5; -- minutes tens (0 to 5)
-    signal h_u : integer range 0 to 9; -- hours units (0 to 9)
-    signal h_d : integer range 0 to 2; -- hours tens (0 to 2)
-
+ENTITY clock IS
+	PORT (
+		clk, rst : IN std_logic;
+		b1, b2, b3, b4 : IN std_logic;
+		d1, d2, d3, d4 : OUT std_logic_vector(6 DOWNTO 0)
+	);
+END clock;
     
-    signal SETTING_MODE : boolean := false;
-    signal CURRENT_DIGIT : integer range 0 to 3; -- digit selection for setting mode
-
-    signal b2_pressed: boolean := false; 
-    signal b3_pressed: boolean := false; 
-
-begin
-
-
-    -- Main clock process
-    p_clock : process (clk, rst)
-        variable hold_b1_time : integer := 0;
-    begin
-        -- RESET
-        if rst = '1' then
-            count_sec <= 0;
-            count_clock <= 0;
-            m_u <= 0;
-            m_d <= 0;
-            h_u <= 0;
-            h_d <= 0;
-            hold_b1_time := 0;
-            SETTING_MODE <= false;
-            CURRENT_DIGIT <= 0;
-
-        elsif rising_edge(clk) then
-        
-            -- If the button has been held for some time 
-            -- and now they are not, release the lock
-            if b2_pressed and b2='0' then b2_pressed <= false; end if;
-            if b3_pressed and b3='0' then b3_pressed <= false; end if;
-
-            
-            -- Check for button 1 press to toggle SETTING_MODE
-            if b1 = '1' then
-                hold_b1_time := hold_b1_time + 1;
-                if hold_b1_time >= 20 then -- simulating a 2 seconds hold
-                    SETTING_MODE <= not SETTING_MODE;
-                    CURRENT_DIGIT <= 0;
-                end if;
-            else
-                hold_b1_time := 0;
-            end if;
-
-            -- Normal mode: count seconds and increment digits accordingly
-            if not SETTING_MODE then
-                -- Update counters: each 10 clock cycles equals 1 second
-                if count_clock = 9 then
-                    count_sec <= count_sec + 1;
-                    count_clock <= 0;
-
-                    -- After 59 seconds, update minutes
-                    if count_sec = 59 then
-                        count_sec <= 0;
-
-                        -- Increment minutes units
-                        if m_u = 9 then
-                            m_u <= 0;
-                            if m_d = 5 then
-                                m_d <= 0;
-                                -- Increment hours units with wrap-around
-                                if h_d = 0 and h_u = 9 then
-                                    h_d <= 1;
-                                    h_u <= 0;
-                                elsif h_d = 1 and h_u = 9 then
-                                    h_d <= 2;
-                                    h_u <= 0;
-                                elsif h_d = 2 and h_u = 3 then
-                                    h_d <= 0;
-                                    h_u <= 0;
-                                else
-                                    h_u <= h_u + 1;
-                                end if;
-                            else
-                                m_d <= m_d + 1;
-                            end if;
-                        else
-                            m_u <= m_u + 1;
-                        end if;
-                    end if;
-                else
-                    count_clock <= count_clock + 1;
-                end if;
-
-            -- Setting mode: adjust the current digit selected by `CURRENT_DIGIT`
-            else
-                if b2 = '1' and b2_pressed = false then
-                    b2_pressed <= true;
-                    CURRENT_DIGIT <= (CURRENT_DIGIT + 1) mod 4;
-                end if;
-
-                if b3 = '1' and b3_pressed = false then
-                    
-                    b3_pressed <= true;
-                    -- Reset seconds to 0 when modifying any digits
-                    count_sec <= 0;
-                    count_clock <= 0;
-                    
-                    case CURRENT_DIGIT is
-                        when 0 => -- Setting minutes units
-                        
-                            if m_u = 9 then m_u <= 0; else m_u <= m_u + 1; end if;
-                        
-                        when 1 => -- Setting minutes decimals
-                            if m_d = 5 then m_d <= 0; else m_d <= m_d + 1; end if;
-                            
-                        when 2 => -- Setting hours units
-                            if h_d = 2 and h_u = 3 then
-                                h_d <= 0; -- Prevent >23
-                                h_u <= h_u +1; 
-                            elsif h_u = 9 then
-                                h_u <= 0;           
-                            else
-                                h_u <= h_u + 1;
-                            end if;
-                            
-                        when 3 => -- Setting hours decimals
-                            if h_d = 2 then
-                                h_d <= 0;
-                            elsif h_d = 1 then
-                                if h_u > 3 then h_u <= 0; end if; -- Prevent >23
-                                h_d <= 2;
-                            else
-                                h_d <= 1; 
-                            end if;
-                        when others =>
-                            null;
-                    end case;
-                end if;
-            end if;
-        end if;
-    end process;
-
-    -- 7-Segment Display process
-    pout : process (h_u, h_d, m_u, m_d)
-    begin
-        -- Display minutes (units) on 7-segment
-        case m_u is
-            when 0 => d1 <= "0000001"; -- 0
-            when 1 => d1 <= "1001111"; -- 1
-            when 2 => d1 <= "0010010"; -- 2
-            when 3 => d1 <= "0000110"; -- 3
-            when 4 => d1 <= "1001100"; -- 4
-            when 5 => d1 <= "0100100"; -- 5
-            when 6 => d1 <= "0100000"; -- 6
-            when 7 => d1 <= "0001111"; -- 7
-            when 8 => d1 <= "0000000"; -- 8
-            when 9 => d1 <= "0000100"; -- 9
-            when others => d1 <= "1111111"; -- blank
-        end case;
-
-        -- Display minutes (tens) on 7-segment
-        case m_d is
-            when 0 => d2 <= "0000001"; -- 0
-            when 1 => d2 <= "1001111"; -- 1
-            when 2 => d2 <= "0010010"; -- 2
-            when 3 => d2 <= "0000110"; -- 3
-            when 4 => d2 <= "1001100"; -- 4
-            when 5 => d2 <= "0100100"; -- 5
-            when others => d2 <= "1111111"; -- blank
-        end case;
-
-        -- Display hours (units) on 7-segment
-        case h_u is
-            when 0 => d3 <= "0000001"; -- 0
-            when 1 => d3 <= "1001111"; -- 1
-            when 2 => d3 <= "0010010"; -- 2
-            when 3 => d3 <= "0000110"; -- 3
-            when 4 => d3 <= "1001100"; -- 4
-            when 5 => d3 <= "0100100"; -- 5
-            when 6 => d3 <= "0100000"; -- 6
-            when 7 => d3 <= "0001111"; -- 7
-            when 8 => d3 <= "0000000"; -- 8
-            when 9 => d3 <= "0000100"; -- 9
-            when others => d3 <= "1111111"; -- blank
-        end case;
-
-        -- Display hours (tens) on 7-segment
-        case h_d is
-            when 0 => d4 <= "0000001"; -- 0
-            when 1 => d4 <= "1001111"; -- 1
-            when 2 => d4 <= "0010010"; -- 2
-            when others => d4 <= "1111111"; -- blank
-        end case;
-    end process;
-end hardware;
+ARCHITECTURE hardware OF clock IS
+	SIGNAL count_sec : INTEGER RANGE 0 TO 59; -- seconds counter (0 to 59)
+	SIGNAL count_clock : INTEGER RANGE 0 TO 9; -- clock cycles counter (0 to 9)
+	SIGNAL m_u : INTEGER RANGE 0 TO 9; -- minutes units (0 to 9)
+	SIGNAL m_d : INTEGER RANGE 0 TO 5; -- minutes tens (0 to 5)
+	SIGNAL h_u : INTEGER RANGE 0 TO 9; -- hours units (0 to 9)
+	SIGNAL h_d : INTEGER RANGE 0 TO 2; -- hours tens (0 to 2)
+	SIGNAL SETTING_MODE : BOOLEAN := false;
+	SIGNAL CURRENT_DIGIT : INTEGER RANGE 0 TO 3; -- digit selection for setting mode
+	SIGNAL b2_pressed : BOOLEAN := false;
+	SIGNAL b3_pressed : BOOLEAN := false;
+BEGIN
+	-- Main clock process
+	p_clock : PROCESS (clk, rst)
+		VARIABLE hold_b1_time : INTEGER := 0;
+	BEGIN
+		-- RESET
+		IF rst = '1' THEN
+			count_sec <= 0;
+			count_clock <= 0;
+			m_u <= 0;
+			m_d <= 0;
+			h_u <= 0;
+			h_d <= 0;
+			hold_b1_time := 0;
+			SETTING_MODE <= false;
+			CURRENT_DIGIT <= 0;
+		ELSIF rising_edge(clk) THEN
+			-- If the button has been held for some time
+			-- and now they are not, release the lock
+			IF b2_pressed AND b2 = '0' THEN
+				b2_pressed <= false;
+			END IF;
+			IF b3_pressed AND b3 = '0' THEN
+				b3_pressed <= false;
+			END IF;
+			-- Check for button 1 press to toggle SETTING_MODE
+			IF b1 = '1' THEN
+				hold_b1_time := hold_b1_time + 1;
+				IF hold_b1_time >= 20 THEN -- simulating a 2 seconds hold
+					SETTING_MODE <= NOT SETTING_MODE;
+					CURRENT_DIGIT <= 0;
+				END IF;
+			ELSE
+				hold_b1_time := 0;
+			END IF;
+			-- Normal mode: count seconds and increment digits accordingly
+			IF NOT SETTING_MODE THEN
+				-- Update counters: each 10 clock cycles equals 1 second
+				IF count_clock = 9 THEN
+					count_sec <= count_sec + 1;
+					count_clock <= 0;
+					-- Update minutes and hours
+					IF count_sec = 59 THEN
+						count_sec <= 0;
+						-- Increment minutes units
+						IF m_u = 9 THEN
+							m_u <= 0;
+							IF m_d = 5 THEN
+								m_d <= 0;
+								-- Hours update logic
+								IF h_d = 2 AND h_u = 3 THEN
+									h_d <= 0;
+									h_u <= 0;
+								ELSIF h_u = 9 THEN
+									h_u <= 0;
+									h_d <= h_d + 1;
+								ELSE
+									h_u <= h_u + 1;
+								END IF;
+							ELSE
+								m_d <= m_d + 1;
+							END IF;
+						ELSE
+							m_u <= m_u + 1;
+						END IF;
+					END IF;
+				ELSE
+					count_clock <= count_clock + 1;
+				END IF;
+				-- Setting mode: adjust the current digit selected by `CURRENT_DIGIT`
+			ELSE
+				IF b2 = '1' AND b2_pressed = false THEN
+					b2_pressed <= true;
+					CURRENT_DIGIT <= (CURRENT_DIGIT + 1) MOD 4;
+				END IF;
+				IF b3 = '1' AND b3_pressed = false THEN
+					b3_pressed <= true;
+					-- Reset seconds to 0 when modifying any digits
+					count_sec <= 0;
+					count_clock <= 0;
+					CASE CURRENT_DIGIT IS
+						WHEN 0 => -- Setting minutes units
+							IF m_u = 9 THEN
+								m_u <= 0;
+							ELSE
+								m_u <= m_u + 1;
+							END IF;
+						WHEN 1 => -- Setting minutes decimals
+							IF m_d = 5 THEN
+								m_d <= 0;
+							ELSE
+								m_d <= m_d + 1;
+							END IF;
+						WHEN 2 => -- Setting hours units
+							IF h_d = 2 AND h_u = 3 THEN
+								h_d <= 0; -- Prevent >23
+								h_u <= 0;
+							ELSIF h_u = 9 THEN
+								h_u <= 0;
+								h_d <= h_d + 1
+								ELSE
+									h_u <= h_u + 1;
+							END IF;
+						WHEN 3 => -- Setting hours decimals
+							IF h_d = 2 THEN
+								h_d <= 0;
+							ELSE
+								h_d <= h_d + 1;
+							END IF;
+						WHEN OTHERS =>
+							NULL;
+					END CASE;
+				END IF;
+			END IF;
+		END IF;
+	END PROCESS;
+	-- 7-Segment Display process
+	pout : PROCESS (h_u, h_d, m_u, m_d)
+	BEGIN
+		-- Display minutes (units) on 7-segment
+		CASE m_u IS
+			WHEN 0 => d1 <= "0000001"; -- 0
+			WHEN 1 => d1 <= "1001111"; -- 1
+			WHEN 2 => d1 <= "0010010"; -- 2
+			WHEN 3 => d1 <= "0000110"; -- 3
+			WHEN 4 => d1 <= "1001100"; -- 4
+			WHEN 5 => d1 <= "0100100"; -- 5
+			WHEN 6 => d1 <= "0100000"; -- 6
+			WHEN 7 => d1 <= "0001111"; -- 7
+			WHEN 8 => d1 <= "0000000"; -- 8
+			WHEN 9 => d1 <= "0000100"; -- 9
+			WHEN OTHERS => d1 <= "1111111"; -- blank
+		END CASE;
+		-- Display minutes (tens) on 7-segment
+		CASE m_d IS
+			WHEN 0 => d2 <= "0000001"; -- 0
+			WHEN 1 => d2 <= "1001111"; -- 1
+			WHEN 2 => d2 <= "0010010"; -- 2
+			WHEN 3 => d2 <= "0000110"; -- 3
+			WHEN 4 => d2 <= "1001100"; -- 4
+			WHEN 5 => d2 <= "0100100"; -- 5
+			WHEN OTHERS => d2 <= "1111111"; -- blank
+		END CASE;
+		-- Display hours (units) on 7-segment
+		CASE h_u IS
+			WHEN 0 => d3 <= "0000001"; -- 0
+			WHEN 1 => d3 <= "1001111"; -- 1
+			WHEN 2 => d3 <= "0010010"; -- 2
+			WHEN 3 => d3 <= "0000110"; -- 3
+			WHEN 4 => d3 <= "1001100"; -- 4
+			WHEN 5 => d3 <= "0100100"; -- 5
+			WHEN 6 => d3 <= "0100000"; -- 6
+			WHEN 7 => d3 <= "0001111"; -- 7
+			WHEN 8 => d3 <= "0000000"; -- 8
+			WHEN 9 => d3 <= "0000100"; -- 9
+			WHEN OTHERS => d3 <= "1111111"; -- blank
+		END CASE;
+		-- Display hours (tens) on 7-segment
+		CASE h_d IS
+			WHEN 0 => d4 <= "0000001"; -- 0
+			WHEN 1 => d4 <= "1001111"; -- 1
+			WHEN 2 => d4 <= "0010010"; -- 2
+			WHEN OTHERS => d4 <= "1111111"; -- blank
+		END CASE;
+	END PROCESS;
+END hardware;
